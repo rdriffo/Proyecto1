1da19c86264fbed7d97f6e48d4e2d52551b00be9
priffo-karrott <patricio@karrott.cl>
2021-11-23 22:38:59 -0300

    Feat: actualizamos las rutas

--- app/views.py
+++ app/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.views       import  APIView
+from rest_framework.response    import  Response
+from rest_framework             import  status
+import requests
+import cx_Oracle
+from helpers import keys as k
+import sys
+import os
 
-# Create your views here.
+
+class get_data(APIView):
+    '''
+    Retornara la data
+    ---------------------------------------------
+    IN: 
+    OUT:
+    '''
+    def post(self, request):
+        print("-------------------------- Retornar data ------------------------------------")
+        
+        rut   =   request.POST["rut"]
+        
+
+        connection = cx_Oracle.connect(k.usuario, k.clave, k.host)
+
+        # cursor = connection.cursor()
+        # cursor.execute("""
+        #     SELECT RUN, NOMBRE 
+        #     FROM IDENTIFICACION""",
+        #     family = 'AP')
+
+        # for id, descripcion in cursor:
+        #     print("Values:", id, descripcion)
+        
+        # cursor.close()
+        # connection.close()
+        
+        return Response({
+                     "status":   "stado",
+                     "data":     "data"
+                 })
---
+++ helpers/keys.py
@@ -0,0 +1,4 @@
+usuario="prueba"
+clave="prueba01"
+host="190.215.50.170"
+db="principal"
